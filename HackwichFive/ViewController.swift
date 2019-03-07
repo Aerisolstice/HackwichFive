c775acf48012e2c8ac69f112ccb559cc974b4f5b
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-03-07 10:31:29 -1000

    Still working on it

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //@IBOutlet weak var numberSlider: UISlider!
+    
+    @IBOutlet var guessingSlider: UISlider!
+    //@IBOutlet weak var buttonDeclaration: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
-    @IBAction func myGuessButtonPressed(_ sender: Any) {
-        let alert = UIAlertController(title: "Hello World!", message: "This is my first alert view.", preferredStyle: .alert)
-        let action = UIAlertAction (title: "Awesome", style: .default, handler: nil)
+    @IBAction func myGuessActionPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Hello World!", message: "This is my first alert view", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
         alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
-        //It's funny because errors and warnings keep popping up all over the place until I typed out everything
     }
+    
+    //@IBAction func myGuessButtonPressed(_ sender: Any) {
+       // let alert = UIAlertController(title: "Hello World!", message: "This is my first alert view", preferredStyle: .alert)
+       // let action = UIAlertAction (title: "Awesome", style: .default, handler: nil)
+       // alert.addAction(action)
+       // present(alert, animated: true, completion: nil)
+        //It's funny because errors and warnings keep popping up all over the place until I typed out everything}
 }
 

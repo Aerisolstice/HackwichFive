63c1e9ca402490d3eaad6b28a60a25c03512ec2a
Aerisolstice <46410704+Aerisolstice@users.noreply.github.com>
2019-03-07 09:52:32 -1000

    Added IBAction

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello World!", message: "This is my first alert view.", preferredStyle: .alert)
+        let action = UIAlertAction (title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    //It's funny because errors and warnings keep popping up all over the place until I typed out everything
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
